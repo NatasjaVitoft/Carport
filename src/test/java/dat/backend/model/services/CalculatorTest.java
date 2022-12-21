b86b276b2fadef090d6573dfbe4a39b3a3ed5867
Oliver Ravnkilde <ravnkilde1995@gmail.com>
2022-12-21 11:16:46 +0100

    test virker

--- src/test/java/dat/backend/model/services/CalculatorTest.java
+++ src/test/java/dat/backend/model/services/CalculatorTest.java
@@ -4,23 +4,33 @@ import dat.backend.model.config.ApplicationStart;
 import dat.backend.model.entities.BillOfMaterialLine;
 import dat.backend.model.exceptions.DatabaseException;
 import dat.backend.model.persistence.ConnectionPool;
+import dat.backend.model.persistence.ItemMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.servlet.ServletException;
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private static ConnectionPool connectionPool;
+
+
     int post = 0;
     int stern = 0;
 
-    private ConnectionPool connectionPool;
 
-    public void init() throws ServletException {
-        this.connectionPool = ApplicationStart.getConnectionPool();
+    @BeforeEach
+    void setUp() throws SQLException {
+        connectionPool = new ConnectionPool();
+        connectionPool.getConnection();
+
     }
+
+
 
     // udregning af stern til front og bag parti
     @Test
@@ -28,13 +38,15 @@ class CalculatorTest {
 
 
         stern = Calculator.calcUnderSternFrontAndBack(0,600,0, connectionPool).getQuantity();
+        System.out.println(connectionPool);
+
 
 
         // sætter forventede antal stern vi skal bruge ud fra vores bredde
-        int expectedStern = 2;
+        int expectedStern = 3;
 
         //sammenligner resultatet af forventet stern og faktisk stern fra udregningen
-        assertEquals(stern, expectedStern);
+        assertEquals(expectedStern,stern);
 
     }
 
